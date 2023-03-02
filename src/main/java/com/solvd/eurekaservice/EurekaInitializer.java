9035a69545c00a19f1fcb57c60282ae653efe550
vpetrovaa <vpetrova.laba@solvd.com>
2023-03-02 10:58:36 +0300

    #1

--- src/main/java/com/solvd/eurekaservice/EurekaInitializer.java
+++ src/main/java/com/solvd/eurekaservice/EurekaInitializer.java
@@ -2,8 +2,10 @@ package com.solvd.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaInitializer {
 
 	public static void main(String[] args) {
