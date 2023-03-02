d9bcee30470f76fd89ae84285599c6c75af26cd1
vpetrovaa <vpetrova.laba@solvd.com>
2023-03-02 03:36:30 +0300

    Generating project structure

---
+++ src/main/java/com/solvd/eurekaservice/EurekaInitializer.java
@@ -0,0 +1,13 @@
+package com.solvd.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaInitializer.class, args);
+	}
+
+}
